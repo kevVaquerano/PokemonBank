efaf695fef8e7a422b2e37408adc81ed3ea22d6f
lau <168582504+HUlauLA@users.noreply.github.com>
2025-03-27 15:58:07 -0600

    intento 2

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
-function irAlMenu() {
-  window.location.href = "html/menu.html";
+function validarPIN(event){
+    event.preventDefault(); //Detiene el envío del formulario
+
+    const pin = document.getElementById("pin").value;
+
+    if(pin == "1234"){
+        window.location.href = "html/menu.html"; //Redirige si el pin es correcto
+    } else {
+        alert("PIN incorrecto"); //Muestra una alerta si el PIN es incorrecto
+    }
 }

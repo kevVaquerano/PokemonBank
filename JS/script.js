f704c9c62bf3df76728930fcd7f14a3d59fbb5b4
lau <168582504+HUlauLA@users.noreply.github.com>
2025-03-28 07:18:19 -0600

    AHORA SI

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ function validarPIN(event){
     const pin = document.getElementById("pin").value;
 
     if(pin == "1234"){
-        window.location.href = "html/menu.html"; //Redirige si el pin es correcto
+        window.location.href = "menu.html"; //Redirige si el pin es correcto
     } else {
         alert("PIN incorrecto"); //Muestra una alerta si el PIN es incorrecto
     }
